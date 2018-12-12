8e88c96ace42f78cfcb153154186c58eb465f2cd
kongaman <christian.kranz@gmail.com>
2018-12-12 13:43:30 +0100

    created init method in Impl-Contructour to have an order in the 'Database'

--- src/main/java/com/ckranz/ws/soap/CustomerOrderWsImpl.java
+++ src/main/java/com/ckranz/ws/soap/CustomerOrderWsImpl.java
@@ -1,12 +1,43 @@
 package com.ckranz.ws.soap;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.bharath.ws.trainings.CreateOrdersRequest;
 import com.bharath.ws.trainings.CreateOrdersResponse;
 import com.bharath.ws.trainings.CustomerOrdersPortType;
 import com.bharath.ws.trainings.GetOrdersRequest;
 import com.bharath.ws.trainings.GetOrdersResponse;
+import com.bharath.ws.trainings.Order;
+import com.bharath.ws.trainings.Product;
 
 public class CustomerOrderWsImpl implements CustomerOrdersPortType {
+	
+	Map<BigInteger, List<Order>> customerOrders = new HashMap<>();
+	int currentId;
+	
+	public CustomerOrderWsImpl() {
+		init();
+	}
+	
+	public void init() {
+		List<Order> orders = new ArrayList<>();
+		Order order = new Order();
+		order.setId(BigInteger.valueOf(1));
+		
+		Product product = new Product();
+		product.setId("1");
+		product.setDescription("IPhone");
+		product.setQuantity(BigInteger.valueOf(3));
+		
+		order.getProduct().add(product);
+		orders.add(order);
+		
+		customerOrders.put(BigInteger.valueOf(++currentId), orders);
+	}
 
 	@Override
 	public GetOrdersResponse getOrders(GetOrdersRequest parameters) {

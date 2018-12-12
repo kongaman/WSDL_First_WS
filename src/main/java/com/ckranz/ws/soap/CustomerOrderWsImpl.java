aba537e624e4d2d005c41e15839f05233256b953
kongaman <christian.kranz@gmail.com>
2018-12-12 10:49:22 +0100

    created class CustomerOrdersWsImpl implementing the auto generated Interface CustomerOrdersPortType

---
+++ src/main/java/com/ckranz/ws/soap/CustomerOrderWsImpl.java
@@ -0,0 +1,23 @@
+package com.ckranz.ws.soap;
+
+import com.bharath.ws.trainings.CreateOrdersRequest;
+import com.bharath.ws.trainings.CreateOrdersResponse;
+import com.bharath.ws.trainings.CustomerOrdersPortType;
+import com.bharath.ws.trainings.GetOrdersRequest;
+import com.bharath.ws.trainings.GetOrdersResponse;
+
+public class CustomerOrderWsImpl implements CustomerOrdersPortType {
+
+	@Override
+	public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

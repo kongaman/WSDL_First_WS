e3a6453c85afc94cf84817bf5972087777e712ba
kongaman <christian.kranz@gmail.com>
2018-12-12 10:13:01 +0100

    Initial - empty Spring Boot project

---
+++ src/main/java/com/ckranz/ws/soap/WsdlFirstWsApplication.java
@@ -0,0 +1,12 @@
+package com.ckranz.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlFirstWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlFirstWsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ckranz/ws/soap/WsdlFirstWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ckranz.ws.soap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsdlFirstWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
